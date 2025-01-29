57703de22b91e3aad4db158585412d2b70448981
Hank <gorillaz1801058@gmail.com>
2025-01-29 16:52:34 +0800

    Initial Commit

---
+++ VisionOS_Windows/VisionOS_WindowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOS_WindowsApp.swift
+//  VisionOS_Windows
+//
+//  Created by 溫家豪 on 2025/1/29.
+//
+
+import SwiftUI
+
+@main
+struct VisionOS_WindowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionOS_WindowsTests/VisionOS_WindowsTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOS_WindowsTests.swift
+//  VisionOS_WindowsTests
+//
+//  Created by 溫家豪 on 2025/1/29.
+//
+
+import Testing
+@testable import VisionOS_Windows
+
+struct VisionOS_WindowsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
